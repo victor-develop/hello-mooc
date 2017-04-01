e46d9448fb60d4aab0758ebd7f3da42fb90be1d3
kinny <victor.connect@gmx.com>
2017-04-01 16:05:44 +0000

    UI optimize

--- js/content.js
+++ js/content.js
@@ -14,7 +14,9 @@ function renderContent() {
             $("<td/>").html(video.video_id).appendTo(item);
             $("<td/>").html(video.name).appendTo(item);
             $("<td/>").html(video.length + "s").appendTo(item);
-            $("<td/>").html("<a href='play.html?video_id=" + video.video_id + "'>Play</a>").appendTo(item);
+            $("<td/>").html("<a href='play.html?video_id=" + video.video_id + "&tracker=clmtrackr'>Play(clmtrackr)</a><br>"+
+                            "<a href='play.html?video_id=" + video.video_id + "&tracker=js_feat'>Play(js_feat)</a>"
+            ).appendTo(item);
             item.appendTo(element);
         })
     })
